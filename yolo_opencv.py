cd238160ba966d490c2f9c9a85c68961e4cb6dcb
Rasmus Schlunsen <rasmus@schlunsen.com>
2023-03-07 21:34:20 +0100

    Fix bug with get_output_layers. Add label printing

--- yolo_opencv.py
+++ yolo_opencv.py
@@ -47,7 +47,7 @@ def get_output_layers(net):
 
     layer_names = net.getLayerNames()
 
-    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 
     return output_layers
 
@@ -92,28 +92,37 @@ def detect(image):
     conf_threshold = 0.5
     nms_threshold = 0.4
 
+    labels = []
+    labels_high_confidence = []
     for out in outs:
         for detection in out:
             scores = detection[5:]
             class_id = np.argmax(scores)
+            label = str(classes[class_id])
+            
             confidence = scores[class_id]
-            if confidence > 0.5:
+            if confidence > 0.1:
+                labels.append(label)
+
+            if confidence > 0.3:
+                labels_high_confidence.append(label)
                 center_x = int(detection[0] * Width)
                 center_y = int(detection[1] * Height)
                 w = int(detection[2] * Width)
                 h = int(detection[3] * Height)
                 x = center_x - w / 2
                 y = center_y - h / 2
+                print(class_id)
                 class_ids.append(class_id)
                 confidences.append(float(confidence))
                 boxes.append([x, y, w, h])
 
 
+    print(labels)
     indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
 
     orgImage = image.copy()
     for i in indices:
-        i = i[0]
         box = boxes[i]
         x = box[0]
         y = box[1]
